15c4a573150bd2c85f729dd6143b77e7f70108c2
serdaryyildiz <yildiz.serdaryy@gmail.com>
2025-09-14 14:07:42 +0300

    electron dependencies

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import configuration from '../config/configuration';
 
 @Module({
   imports: [ConfigModule.forRoot({
-    envFilePath : '../../../.env.dev.local'
+    envFilePath : ['../../../.env.dev.local' , ],
+    isGlobal: true,
+    load : [configuration] , 
   })],
   controllers: [AppController],
   providers: [AppService],
