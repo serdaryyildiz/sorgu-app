72c6004daeb01bc8e91e03ff96978a619f8fc305
serdaryyildiz <yildiz.serdaryy@gmail.com>
2025-09-18 16:37:07 +0300

    postgres connection

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,20 +1,50 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
 import { HealthModule } from 'src/health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath : ['../../../.env.dev.local' , ],
-    isGlobal: true,
-    load : [configuration] , 
-  }),
-    HealthModule,
+  imports: 
+    [
+      ConfigModule.forRoot({
+        isGlobal:true,
+        envFilePath: [
+          path.resolve(__dirname , "../../.env.dev.local"),
+          path.resolve(__dirname , "../../.env.local"),
+          path.resolve(__dirname , "../../.env"),
+
+          path.resolve(process.cwd(), '../.env.dev.local'),
+          path.resolve(process.cwd(), '../.env.local'),
+          path.resolve(process.cwd(), '../.env'),
+
+          
+        ]
+      }),
+
+      TypeOrmModule.forRootAsync({
+        inject:[ConfigService],
+        useFactory: ( config : ConfigService) => ({
+          type: "postgres",
+          host:config.get<string>('HOST'),
+          port:config.get<number>('DB_PORT'),
+          username:config.get<string>('DB_USERNAME'),
+          password:config.get<string>('DB_PASSWORD'),
+          database:config.get<string>('DATABASE'),
+          autoLoadEntities:true,
+          synchronize:true
+        })
+      }),
+
+      HealthModule
+    ],
     
-  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -1,4 +1,9 @@
 export default () => ({
-    port: process.env.PORT ||  3000 ,
-    base_url : process.env.BASE_URL || "http://localhost:3000 "
+    port:process.env.PORT ||  3000 ,
+    base_url :process.env.BASE_URL || "http://localhost:3000",
+    host:process.env.HOST,
+    db_port:Number(process.env.DB_PORT),
+    db_username :process.env.DB_USERNAME,
+    db_password:process.env.DB_PASSWORD,
+    db:process.env.DATABASE,
 });
