597aec5087fd2537c3e9566f9e0c5a6fe67ab454
serdaryyildiz <yildiz.serdaryy@gmail.com>
2025-09-18 15:38:33 +0300

    health module imported

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '../config/configuration';
+import { HealthModule } from 'src/health/health.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath : ['../../../.env.dev.local' , ],
     isGlobal: true,
     load : [configuration] , 
-  })],
+  }),
+    HealthModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/health/health.module.ts
+++ server/src/health/health.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { HealthService } from './health.service';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
+import { HealthController } from './health.controller';
 
 @Module({
   providers: [HealthService],
-  imports: [TerminusModule , HttpModule]
+  imports: [TerminusModule , HttpModule],
+  controllers: [HealthController]
 })
 export class HealthModule {}
