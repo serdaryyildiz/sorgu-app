b74c8f53410a8ac1ecb485852ebfa28813f3df76
serdaryyildiz <yildiz.serdaryy@gmail.com>
2025-09-18 13:48:56 +0300

    npm update

--- server/src/health/health.controller.ts
+++ server/src/health/health.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller , Get} from '@nestjs/common';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+    @Get()
+    getHealth():String{
+        try {
+            return "200 OK";
+        }catch(e){
+            return `Error :\n${(e as Error).message}`;
+        }
+    }
+}
